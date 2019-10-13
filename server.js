03596d950b9d5d344ce885ace174bbfef760f7b2
Tierra <sr.undershaft@gmail.com>
2019-10-12 23:17:28 -0500

    reverting to working state

--- server.js
+++ server.js
@@ -13,14 +13,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(
-  session({
-    secret: "keyboard cat",
-    cookie: { secure: true ,maxAge: 60000 },
-    saveUninitialized: true,
-    resave: false
-  })
-);
+app.use(session({ secret: "keyboard cat", cookie: { maxAge: 60000 } }));
 
 // Handlebars
 app.engine(
